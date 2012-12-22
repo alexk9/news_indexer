17c6a587514909c72f5bb6efd64b8f2cb7d626d9
alexk6 <ukira1@gmail.com>
2012-12-22 09:43:30 +0900

    Initial update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewsIndexer::Application.config.secret_token = '7191c7230db8fbf4dc4b2bea079055aa9fd7f3ba1e60be19fed8fcefffc88ee332d260732cdc7385c3eff5557b6e64e93554ae694be5d05a9a1b40a4d254080e'
